174cab0a868c7d98dc26cd7c9c077f4f03c50d88
DanielFG07 <dan07f@gmail.com>
2022-06-04 01:20:45 +0000

    aplicativo cotação de dólar

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 
 	<View style={styles.container}>
     <View style={styles.bloco}>
-	<Text style={styles.TextoBloco}>Cotação do Dólar: </Text>
+	<Text style={styles.TextoBloco}>Cotação do Dólar:  </Text>
 		  
 		  <TouchableOpacity style={styles.botao}
 			  onPress={carregaCotacao}>
